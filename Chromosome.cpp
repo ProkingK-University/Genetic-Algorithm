364015baba7c6b94636717f00135fb44b32a2a53
Ashley Kapaso <ashleykapaso@gmail.com>
2022-08-02 17:20:46 +0200

    Chromosome Implementation Shell

---
+++ Chromosome.cpp
@@ -0,0 +1,51 @@
+#include "Chromosome.h"
+
+Chromosome::Chromosome(int numGenes, RandomGenerator* rand)
+{
+
+}
+
+Chromosome::Chromosome(Chromosome* chromosome)
+{
+
+}
+
+Chromosome::Chromosome(bool* genes, int numGenes)
+{
+
+}
+
+Chromosome::~Chromosome()
+{
+
+}
+
+double Chromosome::fitness(FitnessFunction* fitnessFunction, Chromosome* chromosome, int numGenes)
+{
+
+}
+
+int Chromosome::getNumGenes()
+{
+
+}
+
+Chromosome* Chromosome::crossOver(Chromosome* c2)
+{
+
+}
+
+Chromosome* Chromosome::mutate()
+{
+
+}
+
+std::string toString()
+{
+
+}
+
+bool* Chromosome::getGenes()
+{
+    
+}
